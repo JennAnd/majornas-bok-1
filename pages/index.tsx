6ee25225444dd3f3ec2a040f7c85b4885a40d382
chrs-m <82442354+chrs-m@users.noreply.github.com>
2022-09-01 15:34:15 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import { SanityClient } from "../Sanity/SanityClient";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
