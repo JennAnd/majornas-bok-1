0d35361275680e1d3c65e692aca9dcaaf8b0d2fa
chrs-m <82442354+chrs-m@users.noreply.github.com>
2022-09-13 14:40:08 +0200

    WIP query

--- Next/Components/Footer/Footer.tsx
+++ Next/Components/Footer/Footer.tsx
@@ -2,51 +2,51 @@ import React, { useEffect, useState } from "react";
 import { SanityClient } from "../../SanityClient";
 import * as S from "./Footer.styles";
 
-export const Footer = () => {
-  const [allCompanyData, setCompanyData] = useState<any[]>();
-  const [allOpeningData, setOpeningData] = useState<any[]>();
+export const Footer = ({ openingHours, companyInfo }) => {
+  // const [allCompanyData, setCompanyData] = useState<any[]>();
+  // const [allOpeningData, setOpeningData] = useState<any[]>();
 
-  useEffect(() => {
-    SanityClient.fetch(`*[_type == 'openingHours']`)
-      .then((data) => setOpeningData(data))
-      .catch(console.error);
-  }, []);
+  // useEffect(() => {
+  //   SanityClient.fetch(`*[_type == 'openingHours']`)
+  //     .then((data) => setOpeningData(data))
+  //     .catch(console.error);
+  // }, []);
 
-  useEffect(() => {
-    SanityClient.fetch(`*[_type == "info"][0]`)
-      .then((data) => setCompanyData(data))
-      .catch(console.error);
-  }, []);
+  // useEffect(() => {
+  //   SanityClient.fetch(`*[_type == "info"][0]`)
+  //     .then((data) => setCompanyData(data))
+  //     .catch(console.error);
+  // }, []);
 
   return (
     <S.Container>
-      {allOpeningData?.length > 0 && (
+      {openingHours?.length > 0 && (
         <S.OpeningsHoursContainer>
           <S.OpeningsHoursTitle>Öppettider</S.OpeningsHoursTitle>
-          <S.OpeningsHoursDays>{allOpeningData[2].day}</S.OpeningsHoursDays>
+          <S.OpeningsHoursDays>{openingHours[2].day}</S.OpeningsHoursDays>
           <S.OpeningsHoursTime>
-            {allOpeningData[2].openingHrs}
+            {openingHours[2].openingHrs}
           </S.OpeningsHoursTime>
-          <S.OpeningsHoursDays>{allOpeningData[1].day}</S.OpeningsHoursDays>
+          <S.OpeningsHoursDays>{openingHours[1].day}</S.OpeningsHoursDays>
           <S.OpeningsHoursTime>
-            {allOpeningData[1].openingHrs}
+            {openingHours[1].openingHrs}
           </S.OpeningsHoursTime>
-          <S.OpeningsHoursDays>{allOpeningData[0].day}</S.OpeningsHoursDays>
+          <S.OpeningsHoursDays>{openingHours[0].day}</S.OpeningsHoursDays>
           <S.OpeningsHoursTime>
-            {allOpeningData[0].openingHrs}
+            {openingHours[0].openingHrs}
           </S.OpeningsHoursTime>
         </S.OpeningsHoursContainer>
       )}
 
       <S.CompanyInfoContainer>
-        <S.CompanyName>{allCompanyData?.companyName}</S.CompanyName>
-        <S.CompanyInfo>{allCompanyData?.address}</S.CompanyInfo>
-        <S.CompanyInfo>{allCompanyData?.zipCode}</S.CompanyInfo>
+        <S.CompanyName>{companyInfo?.companyName}</S.CompanyName>
+        <S.CompanyInfo>{companyInfo?.address}</S.CompanyInfo>
+        <S.CompanyInfo>{companyInfo?.zipCode}</S.CompanyInfo>
         <S.CompanyContact href="tel:+46763414000">
-          {allCompanyData?.phone}
+          {companyInfo?.phone}
         </S.CompanyContact>
         <S.CompanyContact href="mailto:asdf@asdf.com">
-          {allCompanyData?.email}
+          {companyInfo?.email}
         </S.CompanyContact>
       </S.CompanyInfoContainer>
     </S.Container>
