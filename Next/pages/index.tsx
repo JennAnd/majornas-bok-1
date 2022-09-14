626e3b07a47aff1653e1cb70b04266dbef755a6b
LinneaEriksson <Linnea-E@hotmail.com>
2022-09-14 13:01:28 +0200

    Added grid

--- Next/pages/index.tsx
+++ Next/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { Bookgrid } from "../Components/Bookgrid/Bookgrid";
 import { Footer } from "../Components/Footer/Footer";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Link href="sanityTestPage">Sanity test</Link>
+      <Bookgrid />
+
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, veritatis
         mollitia. Doloremque laboriosam dolorum adipisci ullam odit minus nulla
---
+++ Sanity/schemas/book.js
@@ -0,0 +1,30 @@
+import { SiBookstack } from "react-icons/si";
+
+export default {
+  name: "book",
+  title: "Böcker",
+  type: "document",
+  icon: SiBookstack,
+  fields: [
+    {
+      name: "title",
+      title: "Titel",
+      type: "string",
+    },
+
+    {
+      name: "author",
+      title: "Författare",
+      type: "string",
+    },
+
+    {
+      name: "cover",
+      title: "Bokomslag",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
