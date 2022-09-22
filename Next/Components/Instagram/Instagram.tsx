54530eb784722ecede8d29f792184443a5952873
DavisDavisDavis <olizoooom@gmail.com>
2022-09-22 13:22:38 +0200

    fetching instagram image url works!!!!!!!!!!!!!! 💖

--- Next/Components/Instagram/Instagram.tsx
+++ Next/Components/Instagram/Instagram.tsx
@@ -6,9 +6,12 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export const Instagram = () => {
   const username = "yrgo_bokhandel";
+  const token =
+    "IGQVJXdWZAnVWlSTm5rRkpPLUtkR2hSQnU4RjRseGYzYzI0MVdIYVBjQXE4cFJfSjM2dENxRVpUNXJ4M0hZAb3BJTWFSNXZAfUDI1V0RIVlB6TWNIQWZAtMUFkR2JBNGM4Vm1sX0w0ZAmFLUzc2cnU0SUgwYQZDZD";
+  const id = "18046477966366219";
 
   const { data, error } = useSWR(
-    "https://instagram.com/${username}/?__a=1",
+    `https://graph.instagram.com/${id}?fields=id,media_type,media_url,username,timestamp&access_token=${token}`,
     fetcher
   );
 
